719f398a8dc13e820cdda0e3b1aad21175a80d71
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-02-20 07:21:55 -0800

    my header files

--- sort.h
+++ sort.h
@@ -4,15 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Comparison direction macros for bitonic sort*/
+/* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
 
-/*enum bool - Enumeration of boolean values*/
+/* enum bool - enumeration of boolean values */
+
 typedef enum bool
 {
-    false = 0,
-    true
+	false = 0,
+	true
 } bool;
 
 
@@ -25,9 +26,9 @@ typedef enum bool
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /*Helper for swapping*/
@@ -38,10 +39,11 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 /*Regular functions prototypes*/
-
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+
 
 #endif
