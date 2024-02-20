fc0c2d23a5eed49ff1fe09a91960e1197735bd53
nzewunwagrace94 <nzewunwagraceonyinyechi@gmail.com>
2024-02-20 07:56:34 -0800

    my header files

--- sort.h
+++ sort.h
@@ -4,17 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Comparison direction macros for bitonic sort */
-
-#define UP 0
-#define DOWN 1
-
-/* enum bool - Enumeration of boolean values */
-
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
 typedef enum bool
 {
-	false = 0
-	true = 1
+	false = 0,
+	true
 } bool;
 
 
